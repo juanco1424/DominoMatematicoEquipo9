bd88ffc3c2ba87af53a717c166229807daffd314
Juan David Camargo <78766144+juanco1424@users.noreply.github.com>
2024-06-10 09:42:00 -0500

    Update DominoTest.java
    se agregan comentarios a la clase

--- src/test/DominoTest.java
+++ src/test/DominoTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class DominoTest {
     @Test
     public void testConstructorAndGetters() {
+        // instaciamos la clase para tener variables
         Domino domino = new Domino(3, 6, false);
+        // modificamos las variables con metodos set
         assertEquals(3, domino.getLeft());
         assertEquals(6, domino.getRight());
         assertFalse(domino.getInvertir());
@@ -17,10 +19,13 @@ class DominoTest {
 
     @Test
     public void testSetters() {
+        // instaciamos la clase para tener variables
         Domino domino = new Domino(3, 6, false);
+        // modificamos las variables con metodos set
         domino.setLeft(4);
         domino.setRight(5);
         domino.setInvertir(true);
+        // con los metodos assert comparamos los resultados
         assertEquals(4, domino.getLeft());
         assertEquals(5, domino.getRight());
         assertTrue(domino.getInvertir());
@@ -28,8 +33,10 @@ class DominoTest {
 
     @Test
     public void testIsDouble() {
+        // instaciamos la clase para tener variables
         Domino doubleDomino = new Domino(6, 6, false);
         Domino nonDoubleDomino = new Domino(3, 6, false);
+        // con los metodos assert comparamos los resultados
         assertTrue(doubleDomino.isDouble());
         assertFalse(nonDoubleDomino.isDouble());
     }
